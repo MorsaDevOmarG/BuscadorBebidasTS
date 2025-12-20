862b2d2e5de3a734bc4594ba09693b3fb6a8c021
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-12-20 01:04:33 -0600

    Resaltando la pagina actual

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -11,11 +11,33 @@ export default function Header() {
 
           <nav className="flex gap-4">
             {/* to: es la URL que visitará */}
-            <Link to="/" className="text-white uppercase font-bold hover:text-yellow-500 transition-colors">Inicio</Link>
-            <Link to="/favoritos" className="ml-5 text-white font-bold hover:text-yellow-500 transition-colors">Favoritos</Link>
+            {/* <Link to="/" className="text-white uppercase font-bold hover:text-yellow-500 transition-colors">Inicio</Link>
+            <Link to="/favoritos" className="ml-5 text-white font-bold hover:text-yellow-500 transition-colors">Favoritos</Link> */}
+            {/* La diferencia es que NAVLINK tiene un estilo especial para el link activo */}
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Inicio
+            </NavLink>
+            
+            <NavLink
+              to="/favoritos"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
+              Favoritos
+            </NavLink>
           </nav>
         </div>
       </div>
     </header>
-  )
-};
+  );
+}
