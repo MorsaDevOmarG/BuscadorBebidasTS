dbd17bd6f43b86297aace10e7250052c65acc5ad
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-12-20 07:42:14 -0600

    Creando el FORM de BUSQUEDA

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
+import { useMemo } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 
 export default function Header() {
   // Nos ayuda a obtener la ruta actual donde estamos o donde se encuentra el usuario
-  const location = useLocation();
-  console.log(location);
-  console.log(location.pathname); // Nos devuelve la ruta actual
+  const { pathname } = useLocation();
+  // console.log(location);
+  // console.log(location.pathname); // Nos devuelve la ruta actual
+
+  const isHome = useMemo(() => pathname === "/", [pathname]);
+  console.log(isHome);
 
   return (
     <header className="bg-slate-800">
@@ -29,7 +33,7 @@ export default function Header() {
             >
               Inicio
             </NavLink>
-            
+
             <NavLink
               to="/favoritos"
               className={({ isActive }) =>
@@ -42,6 +46,47 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o Ingrediente
+              </label>
+
+              {/* focus: outline: es para cuando el usuario hace clic en el input, no se ponga el color azul */}
+              <input
+                type="text"
+                name="ingredient"
+                id="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                placeholder="Nombre o Ingrediente. Ej: Vodka, Tequila, Café"
+              />
+            </div>
+
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoría
+              </label>
+
+              <select
+                name="ingredient"
+                id="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none bg-white"
+              >
+                <option value="">-- Selecciona --</option>
+              </select>
+            </div>
+
+            <input type="submit" value="Buscar Recetas" className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase" />
+          </form>
+        )}
       </div>
     </header>
   );
