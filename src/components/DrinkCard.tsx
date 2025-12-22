59643a06b8f0d7764198e9fbacf2a234479def7d
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-12-21 23:13:38 -0600

    Finaliznado el codigo para MOSTRAR las BEBIDAS

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -6,8 +6,21 @@ type DrinkCardProps = {
 
 export default function DrinkCard({drink} : DrinkCardProps) {
   return (
-    <div>
-      <h2>{drink.strDrink}</h2>
+    <div className="border shadow-lg">
+      <div className="overflow-hidden">
+        <img src={drink.strDrinkThumb} alt={`Imagen de ${drink.strDrink}`} className="hover:scale-125 transition-transform hover:rounded-2" />
+      </div>
+
+      <div className="p-5">
+        <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+
+        <button
+          type="button"
+          className="bg-orange-400 hover:bg-orange-500 w-full mt-5 p-3 text-white font-bold text-lg"
+        >
+          Ver Receta
+        </button>
+      </div>
     </div>
-  )
+  );
 };
--- src/views/indexPage.tsx
+++ src/views/indexPage.tsx
@@ -10,21 +10,20 @@ export default function indexPage() {
   const hasDrinks = useMemo(() => drinks.drinks.length > 0, [drinks]);
 
   return (
-    <>
+    <div className="container mx-auto max-w-5xl">
       <h1 className="text-6xl font-extrabold">Recetas</h1>
 
       {hasDrinks ? (
-        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
           {drinks.drinks.map((drink) => (
-            <DrinkCard
-              key={drink.idDrink}
-              drink={drink}
-            />
+            <DrinkCard key={drink.idDrink} drink={drink} />
           ))}
-        </>
+        </div>
       ) : (
-        <p className="my-10 text-center text-2xl">No hay recetas aún, utiliza el formulario para buscar recetas</p>
+        <p className="my-10 text-center text-2xl">
+          No hay recetas aún, utiliza el formulario para buscar recetas
+        </p>
       )}
-    </>
+    </div>
   );
 }
