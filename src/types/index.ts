991a66a737aabb8592e79ea5dba7b6ab4f400f99
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-12-21 22:52:57 -0600

    Primeros pasos MOSTRANDO las bebidas

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import {
   CategoriesAPIResponseSchema,
+  DrinkAPIResponse,
   DrinksAPIResponse,
   SearchFilterSchme,
 } from "../utils/recipes-schema";
@@ -8,3 +9,4 @@ import {
 export type Categories = z.infer<typeof CategoriesAPIResponseSchema>;
 export type SearchFilter = z.infer<typeof SearchFilterSchme>;
 export type Drinks = z.infer<typeof DrinksAPIResponse>;
+export type Drink = z.infer<typeof DrinkAPIResponse>;
