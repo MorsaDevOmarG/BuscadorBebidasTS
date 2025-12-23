dc104e887f0fbeb4f47c7a6babdc4123307a79e1
Omar <omar.garcia@morsa.com.mx>
2025-12-23 17:01:58 -0600

    Creando un SLICE para FAVORITOS

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 import { createRecipesSlice, RecipeSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { createFavoritesSlice, FavoritesSliceType } from "./favoriteSlice";
 
 // ...a = toma una copia de todos los argumentos
-export const useAppStore = create<RecipeSliceType>()(devtools( (...a) => ({
-  ...createRecipesSlice(...a)
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(devtools( (...a) => ({
+  ...createRecipesSlice(...a),
+  ...createFavoritesSlice(...a),
 })));
