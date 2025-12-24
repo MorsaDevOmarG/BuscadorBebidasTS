78ce5ad1869b8911cb4175f969b784b08b788ec2
Omar <omar.garcia@morsa.com.mx>
2025-12-24 13:12:30 -0600

    Creando un SLICE para NOTIFICACIONES

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -2,9 +2,11 @@ import { create } from "zustand";
 import { createRecipesSlice, RecipeSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
 import { createFavoritesSlice, FavoritesSliceType } from "./favoriteSlice";
+import { createNotificationSlice, NotificationSliceType } from "./notificationSlice";
 
 // ...a = toma una copia de todos los argumentos
-export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(devtools( (...a) => ({
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType & NotificationSliceType>()(devtools( (...a) => ({
   ...createRecipesSlice(...a),
   ...createFavoritesSlice(...a),
+  ...createNotificationSlice(...a),
 })));
--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import DrinkCard from "../components/DrinkCard";
-import { useAppStore } from "../stores/useAppStore"
+import { useAppStore } from "../stores/useAppStore";
 
 export default function FavoritesPage() {
   const favorites = useAppStore((state) => state.favorites);
@@ -10,24 +10,17 @@ export default function FavoritesPage() {
     <>
       <h1 className="text-6xl font-extrabold">Favoritos</h1>
 
-      {
-        hasFavorites ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
-        {
-          favorites.map(
-            drink => (
-              <DrinkCard
-                key={drink.idDrink}
-                drink={drink}
-              />
-            )
-          )
-        }
-      </div>
-        ) : (
-            <p className="my-10 text-center text-2xl">Los favoritos se mostrarán aquí</p>
-        )
-      }
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {favorites.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
+        </div>
+      ) : (
+        <p className="my-10 text-center text-2xl">
+          Los favoritos se mostrarán aquí
+        </p>
+      )}
     </>
-  )
-};
+  );
+}
