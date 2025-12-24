fd237c994d14be83b2741263e186353c48a5d53c
Omar <omar.garcia@morsa.com.mx>
2025-12-24 13:59:34 -0600

    Añadiendo PERFORMANCE a INDEX

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,12 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import IndexPage from './views/indexPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import IndexPage from './views/indexPage';
 // import FavoritesPage from './views/FavoritesPage';
-import Layout from './layouts/Layout';
-import { lazy, Suspense } from 'react';
+import Layout from "./layouts/Layout";
+import { lazy, Suspense } from "react";
 
 // Esto ayuda a que las páginas no carguen al inicio, solo cuando se ingresa a ella...
 const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
+const IndexPage = lazy(() => import("./views/indexPage"));
 
 export default function AppRouter() {
   return (
@@ -14,7 +15,16 @@ export default function AppRouter() {
         {/* Al poner el ROUTE englobal los otros LAYOUT, es para que te permita usar un COMPONENTE en los demás COMPONENTES sin la necesidad de estar importandolo en cada uno de los COMPONENTES */}
         <Route element={<Layout />}>
           {/* path: es la URL que visitará, al poner index=true se establece como la ruta principal */}
-          <Route path="/" element={<IndexPage />} index />
+          {/* <Route path="/" element={<IndexPage />} index /> */}
+          <Route
+            path="/"
+            element={
+              <Suspense>
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          />
           {/* <Route path="/favoritos" element={<FavoritesPage />} /> */}
           <Route
             path="/favoritos"
@@ -28,4 +38,4 @@ export default function AppRouter() {
       </Routes>
     </BrowserRouter>
   );
-};
+}
